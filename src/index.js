93a79efc112da820708ea7a66452123183a87d81
Ioana Junc <ioanaj11@yahoo.com>
2023-06-07 12:22:57 +0300

    Added drop down menu with 5 locations.Still needs work on update time and sliding effect

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { clearDOMelements } from './clearDOM';
 
 const searchBtn=document.querySelector('button');
 const locationInput=document.querySelector('input');
+const locationDropDownMenu=document.getElementById('locationDropDownMenu');
+const presetLocationsDiv=document.getElementById('presetLocationsDiv');
 
 async function initialDisplay(){
     const weatherData= await getAPIinfo('Oradea');
@@ -12,6 +14,21 @@ async function initialDisplay(){
 }
 
 initialDisplay();
+
+locationDropDownMenu.addEventListener('mouseover', e=>{
+    presetLocationsDiv.classList.remove('invisible');
+})
+
+locationDropDownMenu.addEventListener('mouseout', e=>{
+    presetLocationsDiv.classList.add('invisible');
+})
+
+presetLocationsDiv.addEventListener('mouseover', e =>{
+    presetLocationsDiv.classList.remove('invisible');
+    presetLocationsDiv.addEventListener('mouseout', e =>{
+         presetLocationsDiv.classList.add('invisible')
+    })
+})
 
 searchBtn.addEventListener('click', async e => {
     const weatherData= await getAPIinfo(locationInput.value);
@@ -26,3 +43,12 @@ locationInput.addEventListener('keydown', async e => {
         await clearDOMelements();
         await displayWeather(weatherData);}
 } )
+
+for(let i=1; i<=5; i++){
+    const forecastForLocation=document.getElementById(`forecastForLocation${i}`);
+    forecastForLocation.addEventListener('click', async e =>{
+        const weatherData=await getAPIinfo(forecastForLocation.textContent);
+        await clearDOMelements();
+        await displayWeather(weatherData);
+    })
+}
